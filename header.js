b7cbea093a6ca562851367e55e1cc388fc0188bc
tanmayhinge <hingetanmay@gmail.com>
2025-11-24 12:36:55 -0600

    wrgrgw

---
+++ header.js
@@ -0,0 +1,7 @@
+// Load header component
+fetch('/header.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('header-placeholder').innerHTML = data;
+  })
+  .catch(error => console.error('Error loading header:', error));
